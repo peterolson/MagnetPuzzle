5e6cf62b24b0dd2cb6ecb26950062385ad27e283
Peter Olson <peter.e.c.olson+github@gmail.com>
2018-05-05 17:10:09 -0500

    add solver

--- src/Solve.ts
+++ src/Solve.ts
@@ -1,13 +1,45 @@
-function solve(board : Board) {
-    
-    let movesQueue = [board];
+class Move {
+    public readonly history: Board[];
+    public readonly board: Board;
 
-    while(movesQueue.length) {
-        let move = movesQueue.shift();
-        if(move.isVictory()) {
-            return move;
+    constructor(board: Board, previousMove?: Move) {
+        this.board = board;
+        if (previousMove) {
+            this.history = previousMove.history.concat(board);
+        } else {
+            this.history = [board];
         }
-        
     }
-    
+
+    public isVictory() {
+        return this.board.isVictory();
+    }
+
+    public getAllMoves(encounteredMoves) {
+        return this.board.getAllMoves()
+            .filter(x => !encounteredMoves[x.stringRepresentation])
+            .map(x => {
+                encounteredMoves[x.stringRepresentation] = true;
+                return new Move(x, this)
+            });
+    }
+}
+
+function solve(board: Board) {
+
+    let movesQueue = [new Move(board)];
+    let encounteredMoves = {};
+    encounteredMoves[board.stringRepresentation] = true;
+
+    while (movesQueue.length) {
+        let move = movesQueue.shift();
+
+        if (move.isVictory()) {
+            return move.history;
+        }
+
+        movesQueue = movesQueue.concat(move.getAllMoves(encounteredMoves));
+    }
+
+    return null;
 }
